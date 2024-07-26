3cb89586cd98685484f9d2569db3658c5403f9d5
Sundaram Tiwari <tiwarisundaram01@gmail.com>
2024-07-26 14:02:45 +0530

    creating Main_Layout

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,4 @@
+<!-- Footer -->
+<footer class="bg-dark text-light text-center py-3 mt-4">
+    &copy; 2024 Your Company. All rights reserved.
+</footer>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,36 +1,4 @@
 @extends('master')
-
-@section('header')
-        <!-- Header -->
-        <header>
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="#">Your Logo</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Orders</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Add to Cart</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Sign Up</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Login</a>
-                </li>
-                </ul>
-            </div>
-            </nav>
-        </header>
-@endsection
-
 @section('content')
     <!-- Main Content (Login Page) -->
     <div class="container mt-4">
@@ -57,9 +25,4 @@
     </div>
 @endsection
 
-@section('footer')
-    <!-- Footer -->
-    <footer class="bg-dark text-light text-center py-3 mt-4">
-        &copy; 2024 Your Company. All rights reserved.
-    </footer>
-@endsection
+
